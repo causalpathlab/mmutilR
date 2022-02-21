8c05857b680cd9bffec017863d98a8cd019b5332
Y Park <ypark@bccrc.ca>
2022-02-21 15:38:44 -0800

    vertical cat wrapper

--- R/Wrapper.R
+++ R/Wrapper.R
@@ -75,5 +75,86 @@ read.dense <- function(mtx.file,
                              memory.idx,
                              sub.cols,
                              verbose,
-                             NUM_THREADS)    
+                             NUM_THREADS)
+}
+
+#' A wrapper function that concatenates two files vertically
+#' 
+#' @param top.hdr a file header for the top files
+#' @param bottom.hdr a file header for the bottom files
+#' @param out.hdr output file header
+vcat.sparse <- function(top.hdr, bottom.hdr, out.hdr){
+
+    dir.create(dirname(out.hdr), recursive = TRUE, showWarnings = FALSE)
+
+    out.mtx.file <- paste0(out.hdr, ".mtx.gz")
+    out.cols.file <- paste0(out.hdr, ".cols.gz")
+    out.rows.file <- paste0(out.hdr, ".rows.gz")
+
+    top.mtx.file <- paste0(top.hdr, ".mtx.gz")
+    top.rows.file <- paste0(top.hdr, ".rows.gz")
+    top.cols.file <- paste0(top.hdr, ".cols.gz")
+    bottom.mtx.file <- paste0(bottom.hdr, ".mtx.gz")
+    bottom.rows.file <- paste0(bottom.hdr, ".rows.gz")
+    bottom.cols.file <- paste0(bottom.hdr, ".cols.gz")
+
+    .files <- c(top.mtx.file,
+                top.rows.file,
+                top.cols.file,
+                bottom.mtx.file,
+                bottom.rows.file,
+                bottom.cols.file)
+
+    stopifnot(all(file.exists(.files)))
+
+    top.mtx <- read.sparse(top.mtx.file)
+    bottom.mtx <- read.sparse(bottom.mtx.file)
+
+    .ends.with <- function(x, pat) {
+        .len <- nchar(x)
+        .end <- base::substr(x, .len - nchar(pat) + 1, .len)
+        return(.end == pat)
+    }
+
+    .read.vec <- function(.file) {
+        if(.ends.with(.file, ".gz")) {
+            con <- gzfile(.file)
+            ret <- readLines(con)
+            close(con)
+        } else {
+            ret <- readLines(.file)
+        }
+        return(ret)
+    }
+
+    .write.vec <- function(.vec, .file) {
+        con <- gzfile(.file)
+        cat(.vec, file=con, sep="\n")
+        close(con)
+    }
+
+    top.cols <- .read.vec(top.cols.file)
+    top.rows <- .read.vec(top.rows.file)
+
+    bottom.cols <- .read.vec(bottom.cols.file)
+    bottom.rows <- .read.vec(bottom.rows.file)
+
+    .top.idx <- which(top.cols %in% bottom.cols)
+    .bottom.idx <- match(top.cols[.top.idx], bottom.cols)
+
+    ## no overlapping row names
+    stopifnot(!any(top.rows %in% bottom.rows))
+
+    out.mtx <- rbind(top.mtx[, .top.idx, drop = FALSE],
+                     bottom.mtx[, .bottom.idx, drop = FALSE])
+
+    out.rows <- c(top.rows, bottom.rows)
+    out.cols <- top.cols[.top.idx]
+
+    rcpp_mmutil_write_mtx(out.mtx, out.mtx.file)
+
+    .write.vec(out.cols, out.cols.file)
+    .write.vec(out.rows, out.rows.file)
+
+    list(mtx = out.mtx, rows = out.rows, cols = out.cols)
 }
